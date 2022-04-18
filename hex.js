73ad852e06f1d3932583461083d60cfe1b790f7f
Igaurav31 <91722477+Igaurav31@users.noreply.github.com>
2022-04-18 18:20:10 +0530

    Update hex.js

--- hex.js
+++ hex.js
@@ -1,5 +1,5 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const color = document.querySelector(".color");
+const color = document.querySelector(".color"); //querySelector is use to select class elements whereas by second method we targed element with id
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click",function(){
